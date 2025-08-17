aa0a5a1783dd0bf7f1acd990c24d5a9fa384dc60
theGuardianCode <nickrichard878@gmail.com>
2025-08-17 13:10:32 +1000

    added humanitix scraper

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -3,6 +3,16 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import { Tables } from "../../../../database";
 
+type Event = {
+  name: string | undefined;
+  description: string;
+  image: string | undefined;
+  location: string;
+  datetime: Date | null;
+  tags: string[];
+  ticket_link: string | undefined;
+};
+
 async function getLumaEvents(url: string) {
   const { data } = await axios.get(url);
   const document = cheerio.load(data);
@@ -10,15 +20,15 @@ async function getLumaEvents(url: string) {
   const elements = await document("div.schedule").find(".event-link.content-link")
   const info = await Promise.all(
     elements.map(async (i, x) => {
-      const eventInfo = await readLumaEvent(document, x);
+      const eventInfo = await readLumaEvent(document, x, clubName.trim());
       return eventInfo;
     }
   ));
   
-  return {clubName: clubName.trim(), records: info};
+  return info;
 }
 
-async function readLumaEvent(document: cheerio.CheerioAPI, elem: any) {
+async function readLumaEvent(document: cheerio.CheerioAPI, elem: any, clubName: string) {
   const eventPage = document(elem).attr("href");
 
   const url = `https://lu.ma${eventPage}`;
@@ -31,33 +41,107 @@ async function readLumaEvent(document: cheerio.CheerioAPI, elem: any) {
   const description = page("div.event-about-card").find(".content").find(".spark-content").children().map((i, x) => document(x).text()).toArray().join("\n");
   const image = page("div.cover-image").find("img").attr("src");
 
-  return {name: title, description: description, image: image, location: location, tags: ["UQIES"], ticket_link: url};
+  return {name: title, description: description, image: image, location: location, datetime: null, tags: [clubName], ticket_link: url};
+}
+
+async function getHumanitixEvents(url: string, clubName: string) {
+  const { data } = await axios.get(url);
+  const document = cheerio.load(data);
+
+  const cards = document(".EventList").find("a.EventCard");
+
+  const info = await Promise.all(
+    cards.map(async (i, x) => {
+      const eventInfo = await readHumanitixEvent(document, x, clubName);
+      return eventInfo;
+    })
+  );
+
+  return info;
+}
+
+async function readHumanitixEvent(document: cheerio.CheerioAPI, elem: any, clubName: string) {
+  const title = document(elem).attr("aria-label");
+  const timestamp = document(elem).find("span.date_next").text();
+  const location = document(elem).find("div.location").text();
+
+  // Parse timestamp string e.g. Sun, 24 Aug, 10:30am - 5:30pm AEST to JS date object
+  let builtString = "";
+  const [startTime, endTime] = timestamp.split(" - ");
+  const afternoon = startTime.includes("pm");
+  const includesMinutes = startTime.includes(":");
+
+  builtString = startTime.slice(0, startTime.length - 2);
+  if (!includesMinutes) {
+    builtString += ":00";
+  }
+
+  const time = new Date(Date.parse(builtString));
+  if (afternoon && time.getHours() < 12) {
+    time.setHours(time.getHours() + 12);
+  }
+  time.setFullYear(2025);
+
+  let imageSrc: string | undefined = undefined;
+  const picture = document(elem).find("div.banner").find("picture");
+  if (picture.length) {
+    imageSrc = picture.find("img").attr("src");
+  }
+
+  const eventPage = document(elem).attr("href");
+
+  const { data } = await axios.get(eventPage!);
+  const page = cheerio.load(data);
+
+  const description = page("div.EventDescription").text();
+
+  return {name: title, description: description, image: imageSrc, location: location, datetime: time, tags: [clubName], ticket_link: eventPage};
+}
+
+async function updateDatabase(data: Event[], clubName: string): Promise<{success: boolean, error: any}> {
+  const supabase = await createServiceClient();
+  const {data: clubId, error: clubError} = await supabase.from("clubs").select("id").eq("name", clubName).single();
+
+  if (clubId) {
+    const {data: existingRecords} = await supabase.from("events").select();
+    const existingEvents = existingRecords?.map((r: Tables<"events">) => r.name);
+    const filteredEvents = data.filter((r) => !existingEvents?.includes(r.name!));
+
+    const {data: records, error} = await supabase.from("events").insert(filteredEvents).select();
+
+    if (!error) {
+      records.forEach(async (record: Tables<"events">) => {
+        const {error: hostError} = await supabase.from("host_events").insert({club_id: clubId.id, event_id: record.id});
+        console.log(hostError);
+      });
+
+      return {success: true, error: null};
+    }
+
+    return {success: false, error: error};
+  }
+
+  return {success: false, error: clubError};
 }
 
 export async function GET(request: Request) {
   const supabase = await createServiceClient();
 
-  // UQIES - Luma
-  const {data: clubId} = await supabase.from("clubs").select("id").eq("name", "UQIES").single();
-  const data = await getLumaEvents("https://lu.ma/uqies25");
-  const {data: existingRecords} = await supabase.from("events").select();
-  const existingEvents = existingRecords?.map((r: Tables<"events">) => r.name);
-  const filteredEvents = data.records.filter((r) => !existingEvents?.includes(r.name));
+  // // UQIES - Luma
+  // const uqiesData = await getLumaEvents("https://lu.ma/uqies25");
+  // updateDatabase(uqiesData, "UQIES");
 
-  const {data: records, error} = await supabase.from("events").insert(filteredEvents).select();
-
-  if (error == null) {
-    records!.forEach(async (record: Tables<"events">) => {
-      await supabase.from("host_events").insert({club_id: clubId!.id, event_id: record.id});
-    });
-  } else {
-    return Response.json({
-      status: 400,
-      error: error
-    })
-  }
+  const cozyGamersData = await getHumanitixEvents("https://events.humanitix.com/host/lee-president", "Cosy Gamers Club");
+  const result = await updateDatabase(cozyGamersData, "Cozy Gamers Club");
   
+  if (result.success) {
+    return Response.json({
+      status: 200
+    }); 
+  }
+
   return Response.json({
-    status: 200
+    status: 400,
+    error: result.error
   });
 }
