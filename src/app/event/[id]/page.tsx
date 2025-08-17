0e59a3d8b85604bc0dc0ad78a640f7aa01dd1d4c
theGuardianCode <nickrichard878@gmail.com>
2025-08-17 13:21:00 +1000

    rearranged some ui components

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -19,16 +19,16 @@ export default async function EventDescription({
     return (
         <div>
             {eventData.image ? (<img src={eventData.image} className="w-full h-[300px] object-cover rounded-xl mb-3"/>) : null}
-            <div className="flex flex-row my-2">
-                {eventData.tags?.map((tag: String, index: number) => <Tag key={index} text={tag}/>)}
-            </div>
-
             <div className="flex flex-row items-center justify-between">
                 <h1 className="text-2xl font-bold">{eventData.name}</h1>
                 <LikeButton eventId={eventData.id}/>
             </div>
             <p className="text-xl font-bold">Hosted by {clubNames?.join(", ")}</p>
 
+            <div className="flex flex-row my-2">
+                {eventData.tags?.map((tag: String, index: number) => <Tag key={index} text={tag}/>)}
+            </div>
+            
             <div className="mt-3 font-bold">
                 {eventData.datetime != null ? (<p>🗓️: {new Date(Date.parse(eventData.datetime)).toLocaleString("en-AU", {timeZone: "Australia/Brisbane"})}</p>) : null}
                 <p>📍: {eventData.location}</p>
